645226aed4df0a46df12fb99fb10e25a09f740a2
Kathirvel15 <kathirvel.s@thecareerlabs.com>
2022-11-22 12:43:41 +0530

    Updated

---
+++ src/1stfolder/acs.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./acs.css";
+const greek = () => (
+  //  FIRSTSECTION
+  <section className="ACS-header-section-1">
+    <div className="header-section-inner-logo">
+      <img src="Assets/White.svg" alt="ghj" />
+    </div>
+    <div className="header-section-content-center">
+      <div className="header-section-innercontent-first">
+        <h2>Make The Application Process Easier With Our</h2>
+      </div>
+      <div className="header-section-innercontent-second">
+        <h2>Admissions Consulting Services</h2>
+      </div>
+      <div className="header-section-innerimg">
+        <img src="Assets/b.png" width="35%" alt="" />
+      </div>
+    </div>
+  </section>
+);
+export default greek;
