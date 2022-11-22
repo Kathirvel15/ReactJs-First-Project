b2031cbe797627b477988f42f041d79f3b72eb86
Kathirvel15 <kathirvel.s@thecareerlabs.com>
2022-11-22 14:09:52 +0530

    Section-3 Added

--- src/1stfolder/acs.js
+++ src/1stfolder/acs.js
@@ -45,6 +45,115 @@ const greek = () => (
         <p>The process in general involves the following steps:</p>
       </div>
     </section>
+    {/*THIRD SECTION*/}
+    <section class="ACS-section-3">
+      <div class="ACS-section-2-image-section">
+        <div class="ACS-section-2-image-section-innerpart-1">
+          <h1>01</h1>
+        </div>
+        <div class="ACS-section-2-image-section-innerpart-2">
+          <h2>Writing English proficiency test</h2>
+          <p>
+            If you are an international aspirant from a country that does not
+            have English as its first language, you are usually required to take
+            the test of English proficiency. The common ones are TOEFL and
+            IELTS.
+          </p>
+        </div>
+      </div>
+      <div class="ACS-section-3-image-section">
+        <div class="ACS-section-3-image-section-innerpart-2">
+          <h2>Shortlisting universities</h2>
+          <p>
+            Carefully selecting the universities based on your profile, area of
+            interest, market value etc.
+          </p>
+        </div>
+        <div class="ACS-section-3-image-section-innerpart-1">
+          <h1>02</h1>
+        </div>
+      </div>
+
+      <div class="ACS-section-2-image-section">
+        <div class="ACS-section-2-image-section-innerpart-1">
+          <h1>03</h1>
+        </div>
+        <div class="ACS-section-2-image-section-innerpart-2">
+          <h2>Building your profile</h2>
+          <p>
+            Everything comes into account. Your grades, your volunteering
+            activities, skills and knowledge.
+          </p>
+        </div>
+      </div>
+      <div class="ACS-section-3-image-section">
+        <div class="ACS-section-3-image-section-innerpart-2">
+          <h2>Writing a statement of purpose</h2>
+          <p>
+            Graduate/B-Schools usually require an essay, outlining your
+            interests and reasons behind pursuing studies.
+          </p>
+        </div>
+        <div class="ACS-section-3-image-section-innerpart-1">
+          <h1>04</h1>
+        </div>
+      </div>
+
+      <div class="ACS-section-2-image-section">
+        <div class="ACS-section-2-image-section-innerpart-1">
+          <h1>05</h1>
+        </div>
+        <div class="ACS-section-2-image-section-innerpart-2">
+          <h2>Getting letters of recommendation</h2>
+          <p>
+            EYou might be asked to provide letters of recommendation (usually 3)
+            from either your professors during your undergraduate studies or
+            from your colleagues if you are a working professional. The
+            recommendation process is now online in most of the university
+            applications.
+          </p>
+        </div>
+      </div>
+      <div class="ACS-section-3-image-section">
+        <div class="ACS-section-3-image-section-innerpart-2">
+          <h2>Well-Crafted Essays</h2>
+          <p>
+            Each school has its own set of essay questions. You must allow
+            adequate time to draft a quality essay, obtain feedback on that
+            essay, and revise the essay. You might need to go through multiple
+            cycles of this process.
+          </p>
+        </div>
+        <div class="ACS-section-3-image-section-innerpart-1">
+          <h1>06</h1>
+        </div>
+      </div>
+
+      <div class="ACS-section-2-image-section">
+        <div class="ACS-section-2-image-section-innerpart-1">
+          <h1>07</h1>
+        </div>
+        <div class="ACS-section-2-image-section-innerpart-2">
+          <h2>Completing the online application process</h2>
+          <p>
+            The application process for most of the schools is online. The
+            application fee varies depending upon the university.
+          </p>
+        </div>
+      </div>
+      <div class="ACS-section-3-image-section">
+        <div class="ACS-section-3-image-section-innerpart-2">
+          <h2>Student visa</h2>
+          <p>
+            If you are an international student, you will need to get a student
+            visa. This is the final step.
+          </p>
+        </div>
+        <div class="ACS-section-3-image-section-innerpart-1">
+          <h1>08</h1>
+        </div>
+      </div>
+    </section>
   </>
 );
 export default greek;
