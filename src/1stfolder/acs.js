1907a7c93b2a2434e2c45cf55724746ddb6fea19
Kathirvel15 <kathirvel.s@thecareerlabs.com>
2022-11-22 13:07:12 +0530

    Section Added

--- src/1stfolder/acs.js
+++ src/1stfolder/acs.js
@@ -1,22 +1,50 @@
 import React from "react";
 import "./acs.css";
 const greek = () => (
-  //  FIRSTSECTION
-  <section className="ACS-header-section-1">
-    <div className="header-section-inner-logo">
-      <img src="Assets/White.svg" alt="ghj" />
-    </div>
-    <div className="header-section-content-center">
-      <div className="header-section-innercontent-first">
-        <h2>Make The Application Process Easier With Our</h2>
+  <>
+    {/*FIRSTSECTION*/}
+    <section className="ACS-header-section-1">
+      <div className="header-section-inner-logo">
+        <img src="Assets/White.svg" alt="ghj" />
       </div>
-      <div className="header-section-innercontent-second">
-        <h2>Admissions Consulting Services</h2>
+      <div className="header-section-content-center">
+        <div className="header-section-innercontent-first">
+          <h2>Make The Application Process Easier With Our</h2>
+        </div>
+        <div className="header-section-innercontent-second">
+          <h2>Admissions Consulting Services</h2>
+        </div>
+        <div className="header-section-innerimg">
+          <img src="Assets/b.png" width="35%" alt="" />
+        </div>
       </div>
-      <div className="header-section-innerimg">
-        <img src="Assets/b.png" width="35%" alt="" />
+    </section>
+    {/* SECOND SECTION */}
+    <section class="ACS-section-2">
+      <p>
+        What happens next after giving GMAT or GRE? Worried about your Essays
+        and SOPs! This is where ACS comes into picture. ACS stands for
+        Admissions Consulting Services. In this service, expert consultants
+        study your profile and then match the profile to the perfect
+        B-schools/grad schools for you based on various class profiles for these
+        schools. Experts also make sure that the essays and CV/resume, and any
+        other document to be submitted with your application show the best
+        version of you. If you are planning for an MBA , Masters or Masters in
+        Management admission, This service makes your application process easier
+        and smooth.
+      </p>
+      <div class="ACS-section-heading">
+        <h2>Start your process before it's late</h2>
       </div>
-    </div>
-  </section>
+      <p>
+        Ideally one should begin the process at least 9-12 months before the
+        intake year you are planning to apply. However this also depends on the
+        school chosen and rounds applied to.
+      </p>
+      <div class="ACS-section-2-bold-heading">
+        <p>The process in general involves the following steps:</p>
+      </div>
+    </section>
+  </>
 );
 export default greek;
